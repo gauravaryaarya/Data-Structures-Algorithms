87f76cf0ce898871e27a768a6309c1f9f89acc37
Gaurav Arya <144447573+gauravaryaarya@users.noreply.github.com>
2024-10-18 21:05:27 +0530

    2 patterns added

--- DSA/Patterns/squarePattern2.cpp
+++ DSA/Patterns/squarePattern2.cpp
@@ -1,23 +1,24 @@
-/*  1 2 3 4
-    1 2 3 4
-    1 2 3 4
-    1 2 3 4 */
-
-/*  * * * *
- * * * *
- * * * *
- * * * *  */
+/*
+    A B C D
+    A B C D
+    A B C D
+    A B C D
+*/
 #include <iostream>
 using namespace std;
 
 int main()
 {
     int n = 4;
+
     for (int i = 1; i <= n; i++)
     {
+        char ch = 'A';
         for (int j = 1; j <= n; j++)
         {
-            cout << j << " "; // 2. cout << "* ";
+
+            cout << ch << " ";
+            ch++;
         }
         cout << endl;
     }
