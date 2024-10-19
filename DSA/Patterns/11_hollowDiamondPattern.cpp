d23ba0dd7d6c1693925ac0808a1a14a1662286f5
Gaurav Arya <144447573+gauravaryaarya@users.noreply.github.com>
2024-10-19 19:11:57 +0530

    pattern edited

--- DSA/Patterns/11_hollowDiamondPattern.cpp
+++ DSA/Patterns/11_hollowDiamondPattern.cpp
@@ -1,11 +1,11 @@
 /*
- *
- *   *
- *      *
- *        *
- *      *
- *    *
- *
+
+      *
+   *     *
+ *          *
+    *     *
+       *
+
  */
 
 #include <iostream>
@@ -14,12 +14,32 @@ int main()
 {
     int n = 4;
     // UPPER HALF
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
+        // spaces
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        // print star
+        cout << "*";
+
+        if (i != 0)
+        {
+            // spaces
+            for (int k = 0; k < 2 * i - 1; k++)
+            {
+                cout << " ";
+            }
+            // print star
+            cout << "*";
+        }
+
+        cout << endl;
     }
     // LOWER HALF
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n - 1; i++)
     {
-    }
+        }
     return 0;
 }
