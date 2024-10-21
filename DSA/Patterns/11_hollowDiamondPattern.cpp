5864043d4901c1ef2c9ba05b80de881324041573
Gaurav Arya <144447573+gauravaryaarya@users.noreply.github.com>
2024-10-22 01:04:03 +0530

    patterns completed

--- DSA/Patterns/11_hollowDiamondPattern.cpp
+++ DSA/Patterns/11_hollowDiamondPattern.cpp
@@ -7,39 +7,55 @@
        *
 
  */
+// spaces- n-i-1
+// one star
+// spaces (odd)- 2*i-1 , 2*(n-2-i)-1
+// one star
+// first and last row - one star
 
 #include <iostream>
 using namespace std;
 int main()
 {
-    int n = 4;
-    // UPPER HALF
-    for (int i = 0; i < n; i++)
-    {
-        // spaces
-        for (int j = 0; j < n - i - 1; j++)
-        {
+   int n = 4;
+
+   // upper half
+   for (int i = 0; i < n; i++)
+   {
+      for (int k = 0; k < n - i - 1; k++)
+      {
+         cout << " ";
+      }
+
+      cout << "*";
+
+      if (i > 0)
+      {
+         for (int j = 0; j < 2 * i - 1; j++)
+         {
             cout << " ";
-        }
-        // print star
-        cout << "*";
-
-        if (i != 0)
-        {
-            // spaces
-            for (int k = 0; k < 2 * i - 1; k++)
-            {
-                cout << " ";
-            }
-            // print star
-            cout << "*";
-        }
-
-        cout << endl;
-    }
-    // LOWER HALF
-    for (int i = 0; i < n - 1; i++)
-    {
-        }
-    return 0;
+         }
+         cout << "*";
+      }
+      cout << endl;
+   }
+   // lower half
+   for (int i = 0; i < n - 1; i++)
+   {
+      for (int k = 0; k < i + 1; k++)
+      {
+         cout << " ";
+      }
+      cout << "*";
+      if (i != n - 2)
+      {
+         for (int j = 0; j < 2 * (n - 2 - i) - 1; j++)
+         {
+            cout << " ";
+         }
+         cout << "*";
+      }
+      cout << endl;
+   }
+   return 0;
 }
