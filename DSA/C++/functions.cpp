df20f1ccbe1a680b7f9b72e13d607c365b185a2d
Gaurav Arya <144447573+gauravaryaarya@users.noreply.github.com>
2024-10-23 00:37:38 +0530

    functions complete

--- DSA/C++/functions.cpp
+++ DSA/C++/functions.cpp
@@ -43,11 +43,84 @@ int factorial(int n)
     return fac;
 }
 
+// calculate sum of digits of a number
+int sumdigit(int n)
+{
+    int lastdigit = n % 10;
+    int sum = 0;
+    for (int i = 0; n > 0; i++)
+    {
+        sum = sum + lastdigit;
+        n = n / 10;
+        lastdigit = n % 10;
+    }
+
+    return sum;
+}
+
+// calculate nCr binomial coefficient for n & r
+int binomialcoeff(int n, int r)
+{
+    // we have already created factorial function so->
+    int nfact = factorial(n);
+    int rfact = factorial(r);
+    int binomial = nfact / (rfact * factorial(n - r));
+    return binomial;
+}
+
+// WAF to check if a number is prime or not
+bool isPrime(int n)
+{
+    bool primecheck = true;
+    for (int i = 2; i <= n - 1; i++)
+    {
+        if (n % i == 0)
+        {
+            primecheck = false;
+            break;
+        }
+        else
+        {
+            primecheck = true;
+        }
+    }
+    return primecheck;
+}
+
+// WAF to print all prime numbers from 1 to N
+void printPrime(int n)
+{ // using function isPrime
+    for (int i = 2; i <= n; i++)
+    {
+        if (isPrime(i))
+        {
+            cout << i << " ";
+        }
+    }
+}
+
+// WAF to print nth fibonacci number 0,1,1,2,3,5,8....
+int fib(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+
 int main()
 {
     cout << sum(5, 6) << endl;
     cout << minimum(100, 79) << endl;
     cout << sum(10) << endl;
     cout << factorial(5) << endl;
+    cout << sumdigit(123) << endl;
+    cout << binomialcoeff(6, 3) << endl;
+    cout << isPrime(7) << endl;
+    printPrime(10);
+    cout << endl;
+    cout << fib(6) << endl;
+
     return 0;
 }
